2bcbd03103548367fe336efcf8807726bc3a9e1a
xiufu.wang <407399291@qq.com>
2018-03-07 15:31:18 +0800

    新增首页滚动显示动画

--- src/themes/index.component.ts
+++ src/themes/index.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, OnDestroy, ChangeDetectorRef, NgZone, ChangeDetectionStrategy } from '@angular/core';
+import { Component, OnInit, OnDestroy, ChangeDetectorRef, NgZone, ChangeDetectionStrategy, ElementRef, AfterViewInit } from '@angular/core';
 import { Action } from '../app/beans/Action';
 import { ActionType } from '../app/beans/ActionType';
 import { AppComponent } from '../app/app.component';
@@ -10,13 +10,16 @@ declare let $: any;
     selector: 'app-index',
     templateUrl: `./index.component.html`
 })
-export class IndexComponent implements OnInit {
+export class IndexComponent implements OnInit, AfterViewInit, OnDestroy{
 
     private unsubscribeScroll: any;
 
     _scrolled: Boolean = false;
 
+    private _wow: any;
+
     constructor(
+        private el: ElementRef,
         private appComponent: AppComponent,
         private scrollDispatcher: ScrollDispatcher,
         private checkRef: ChangeDetectorRef,
@@ -24,10 +27,24 @@ export class IndexComponent implements OnInit {
     ) { }
 
     ngOnInit() {
-        this.unsubscribeScroll = this.unsubscribeScroll = this.scrollDispatcher.scrolled(200).subscribe(() => {
+        this.unsubscribeScroll = this.unsubscribeScroll = this.scrollDispatcher.scrolled().subscribe(() => {
             this.updateScrollStatus();
          });
      }
+
+    ngAfterViewInit(){
+        this._wow = new WOW({
+            boxClass: 'index-wow',
+            offset: 10
+        });
+        this._wow.init();
+    }
+
+    ngOnDestroy() {
+        if (this._wow) {
+            this._wow.stop();
+        }
+    }
 
      private updateScrollStatus(){
         this._ngZone.run(() => {
--- src/typings.d.ts
+++ src/typings.d.ts
@@ -1,5 +1,7 @@
 /* SystemJS module definition */
 declare var module: NodeModule;
+declare var WOW: any;
+declare var $: any;
 interface NodeModule {
   id: string;
 }
